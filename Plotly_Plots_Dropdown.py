6d093cd1815cd1c8c2ed321282d3f8013e3f4afc
Eduardo Hernandez <86838594+edtm@users.noreply.github.com>
2021-08-19 16:56:02 -0500

    Update Plotly_Plots_Dropdown.py

--- Plotly_Plots_Dropdown.py
+++ Plotly_Plots_Dropdown.py
@@ -92,6 +92,37 @@ fig.add_annotation(
             yref='paper',
             xanchor='left',
             text="df2<br>Name");
+
+# Add range slider
+fig.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label='1m',
+                     step='month',
+                     stepmode='backward'),
+                dict(count=6,
+                     label='6m',
+                     step='month',
+                     stepmode='backward'),
+                dict(count=1,
+                     label='YTD',
+                     step='year',
+                     stepmode='todate'),
+                dict(count=1,
+                     label='1y',
+                     step='year',
+                     stepmode='backward'),
+                dict(step='all')
+            ]),
+        ),
+        rangeslider=dict(
+            visible=True
+        ),
+        type='date'
+    )
+)
             
 
 print("%html " + plotly.io.to_html(fig, include_plotlyjs=True))
