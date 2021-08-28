465814acf408f88fd822da5f035552199f4bf121
Eduardo Hernandez <86838594+edtm@users.noreply.github.com>
2021-08-28 00:08:41 -0500

    Update Timeseries_Integral.py

--- Timeseries_Integral.py
+++ Timeseries_Integral.py
@@ -39,28 +39,7 @@ df=pd.concat(df)
 ####################### REPLACE VIEW UUID ##################################
 
 
-dfs = dict(tuple(df.groupby([df.index.month, df.index.year])))
-
-ValueList=list()
-TagList=list()
-DateList=list()
-
-for tag in df.columns:
-    for i in dfs:
-        y1 = np.array(dfs[i][tag])
-        x1 = np.arange(len(dfs[i][tag]))
-        R = round(scipy.integrate.trapz(y1,x1)/1440,1)
-        ValueList.append(R)
-        TagList.append(tag)
-        DateList.append(i)
-        
-
-SummaryTable = pd.DataFrame({'month-year': DateList, 'tags': TagList, 'values': ValueList})
-SummaryTable = SummaryTable.pivot(index='month-year', columns='tags', values='values')
-SummaryTable['index']=SummaryTable.index
-SummaryTable['index'] =  pd.to_datetime(SummaryTable['index'], format='(%m, %Y)')
-SummaryTable = SummaryTable.set_index('index').sort_index()
-
+SummaryTable = round(df.resample('M').apply(scipy.integrate.trapz)/1440,1)
 
 df_formatted = SummaryTable.style.set_properties(**{'background-color': 'white'})
 
