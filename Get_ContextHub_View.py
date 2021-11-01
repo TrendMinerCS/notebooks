714f24568e000aae7eb27c91dcf4fcff915b8cf5
Eduardo Hernandez <86838594+edtm@users.noreply.github.com>
2021-11-01 16:39:47 -0500

    Update Get_ContextHub_View.py

--- Get_ContextHub_View.py
+++ Get_ContextHub_View.py
@@ -31,33 +31,43 @@ client = TrendMinerClient("{TM_TOKEN.password}", tm_url)
 
 token = '{TM_TOKEN.password}'
 auth_header    = {'Authorization':("Bearer " + token)}
-endpoint = 'context/view/' + viewID + '/enriched'
 
+#Get CH view information
+endpoint = 'context/view/' + viewID + '/enriched'
 r_context_view = requests.get(tm_url + endpoint, headers = auth_header)
 r_context_view_json = r_context_view.json()
 
-endpoint= "context/item/search?size=100&useTimeSeriesIdentifier=true"  #Get last 100 items of CH View
+#Get number of pages of CH items
+endpoint= "context/item/search?size=25"
 body = {"filters": r_context_view_json['data']['filters']}
 headers = {'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'}
+r_context_item_pages = requests.post(tm_url + endpoint, headers=headers, json=body)
+totalPages = r_context_item_pages.json()['page']['totalPages']
 
-r_context_items = requests.post(tm_url + endpoint, headers=headers, json=body)
-r_context_items_json = r_context_items.json()['content']
-
-ContextItemList = []
-for context_item in r_context_items_json:
-
-    (context_item['fields'])['startDate'] = context_item['events'][0]['occurred']
-   
-    if len(context_item['events']) != 2:
-        (context_item['fields'])['endDate'] = ''    
-    else:
-        (context_item['fields'])['endDate'] = context_item['events'][1]['occurred']
+#For each page, get the all context items
+endpoint= "context/item/search?size=25&page="
+r_context_items_page_list = []
+for i in range(1, totalPages+1, 1):
+    r_context_items = requests.post(tm_url + endpoint + str(i), headers=headers, json=body)
+    r_context_items_page_list.append(r_context_items.json()['content'])
     
-    (context_item['fields'])['keywords'] = context_item['keywords']
-    (context_item['fields'])['Description'] = context_item['description']
-    (context_item['fields'])['ItemType'] = context_item['type']['name']
-    (context_item['fields'])['Component'] = context_item['components'][0]['name']
-    ContextItemList.append(context_item['fields'])
+
+#For each page in list, get context items and arrange in simple array
+ContextItemList = []
+for itemPage in r_context_items_page_list:
+    for context_item in itemPage:
+        (context_item['fields'])['startDate'] = context_item['events'][0]['occurred']
+       
+        if len(context_item['events']) != 2:
+            (context_item['fields'])['endDate'] = ''    
+        else:
+            (context_item['fields'])['endDate'] = context_item['events'][1]['occurred']
+        
+        (context_item['fields'])['keywords'] = context_item['keywords']
+        (context_item['fields'])['Description'] = context_item['description']
+        (context_item['fields'])['ItemType'] = context_item['type']['name']
+        (context_item['fields'])['Component'] = context_item['components'][0]['name']
+        ContextItemList.append(context_item['fields'])
 
 
 df = pd.DataFrame(ContextItemList)
