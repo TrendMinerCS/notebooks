9d69326ad92271d770284631962a41e221f99551
Eduardo Hernandez <86838594+edtm@users.noreply.github.com>
2022-03-01 19:24:08 -0600

    Create LogisticRegression.py

---
+++ categorization/LogisticRegression.py
@@ -0,0 +1,98 @@
+################### INPUT ###################
+
+url = "https://dsitl-demo.trendminer.cloud/"
+
+################### INPUT ###################
+
+# Suggested data science package imports
+import pandas as pd
+import numpy as np
+
+from datetime import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+
+# TrendMiner package import
+import trendminer
+from trendminer.trendminer_client import TrendMinerClient
+
+# Create TrendMiner API object
+client = TrendMinerClient("{TM_TOKEN.password}", url)
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+#import mlflow
+#import mlflow.sklearn
+#from mlflow.models.signature import infer_signature
+
+URL = 'https://raw.githubusercontent.com/TrendMiner/notebooks/main/data/diabetes.csv'
+col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+pima = pd.read_csv(URL, header=None, names=col_names)
+
+pima.head()
+
+#split dataset in features and target variable
+feature_cols = ['pregnant', 'insulin', 'bmi', 'age','glucose','bp','pedigree']
+X = pima[feature_cols] # Features
+y = pima.label # Target variable
+
+# split X and y into training and testing sets
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
+
+# import the class
+from sklearn.linear_model import LogisticRegression
+
+# instantiate the model (using the default parameters)
+logreg = LogisticRegression()
+
+# fit the model with data
+logreg.fit(X_train,y_train)
+
+# predicted values
+y_pred=logreg.predict(X_test)
+
+# import the metrics class
+from sklearn import metrics
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+cnf_matrix
+
+# import required modules
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class_names=[0,1] # name  of classes
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+
+# create heatmap
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu" ,fmt='g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+
+plt.show()
+
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Precision:",metrics.precision_score(y_test, y_pred))
+print("Recall:",metrics.recall_score(y_test, y_pred))
+
+y_pred_proba = logreg.predict_proba(X_test)[::,1]
+fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba)
+auc = metrics.roc_auc_score(y_test, y_pred_proba)
+plt.plot(fpr,tpr,label="data 1, auc="+str(auc))
+plt.legend(loc=4)
+plt.show()
