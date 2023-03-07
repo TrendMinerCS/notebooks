11fc8691b734de062d54853ee365ad3cab370ee2
wodaniels <86838527+wodaniels@users.noreply.github.com>
2023-03-07 20:57:50 +0100

    Update anomaly_detection.py

--- MLHub/anomaly_detection.py
+++ MLHub/anomaly_detection.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from pprint import pprint
 
 from trendminer.trendminer_client import TrendMinerClient
 from trendminer.views.views import Views
