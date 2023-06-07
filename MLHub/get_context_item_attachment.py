4bb80fe7c5d2364075847ba44a76a6aa5a8b56f0
Eduardo Hernandez <86838594+edtm@users.noreply.github.com>
2023-06-07 14:58:11 -0500

    Create get_context_item_attachment.py

---
+++ MLHub/get_context_item_attachment.py
@@ -0,0 +1,36 @@
+# Suggested data science package imports
+import pandas as pd
+import numpy as np
+
+from datetime import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+
+# TrendMiner package import
+import trendminer
+from trendminer.trendminer_client import TrendMinerClient
+
+import os
+
+token = os.environ["KERNEL_USER_TOKEN"]
+serverUrl = os.environ["KERNEL_SERVER_URL"]
+
+# Create TrendMiner API object
+client = TrendMinerClient(token, serverUrl)
+
+import requests
+import json
+
+auth_header = {'Authorization': 'Bearer ' + token}
+
+
+def get_contextItem_attachment(attachmentEndpoint):
+    
+    r = requests.get(f'{serverUrl}/context{attachmentEndpoint}', headers=auth_header)
+        
+    return r
+  
+  
+attachmentEndpoint = "/data/046cb744-3690-4a32-8c9c-e171cb906066/attachments/d153bee5-cc86-4546-a217-423eb26bb024/download"
+r = get_contextItem_attachment(attachmentEndpoint)
+pmml_string = str(r.content, 'utf-8')
