9b7369f19bda86d7a6403e5a7fbfcf5857fc8189
Eduardo Hernandez <86838594+edtm@users.noreply.github.com>
2023-07-10 10:57:38 -0500

    Update get_context_item_attachment.py

--- MLHub/get_context_item_attachment.py
+++ MLHub/get_context_item_attachment.py
@@ -29,8 +29,8 @@ def get_contextItem_attachment(attachmentEndpoint):
     r = requests.get(f'{serverUrl}/context{attachmentEndpoint}', headers=auth_header)
         
     return r
-  
-  
+
+# the attachment endpoint below is an example. Make sure you modify for the specific context item and attached file.
 attachmentEndpoint = "/data/046cb744-3690-4a32-8c9c-e171cb906066/attachments/d153bee5-cc86-4546-a217-423eb26bb024/download"
 
 r = get_contextItem_attachment(attachmentEndpoint)
