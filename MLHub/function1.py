c8088896b1a8900f5a5fc461076a4ab6cfe2f0d5
Eduardo Hernandez <86838594+edtm@users.noreply.github.com>
2023-10-18 10:40:47 -0500

    Delete MLHub/function1.py

--- MLHub/function1.py
+++
@@ -1,7 +0,0 @@
-def get_tag_details(tagname):
-    
-    params = {'tagName': tagname}
-    
-    r = requests.get(f'{serverUrl}/hps/api/tags/details', params=params, headers=auth_header)
-    
-    return r.json()
